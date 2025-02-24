cde0f09d25e3b0f34a71014f8766768260b30ab0
Md Rihan NJ <rihannjna@gmail.com>
2025-02-24 19:22:27 +0600

    webhook setting

--- src/app/api/webhooks/route.tsx
+++ src/app/api/webhooks/route.tsx
@@ -49,6 +49,10 @@ export async function POST(req: Request) {
 
   // Do something with payload
   // For this guide, log payload to console
+  const { id } = evt.data;
+  const eventType = evt.type;
+  console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
+  console.log("Webhook payload:", body);
 
   if (evt.type === "user.created") {
     console.log("userId:", evt.data.id);
