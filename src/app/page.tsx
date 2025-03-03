68d62634a2e8e7d469e9eeb9ce53638a5c716b21
Md Rihan NJ <rihannjna@gmail.com>
2025-03-03 22:26:21 +0600

    Removing Ts complete

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
-      <h1>At Taleem</h1>
-    </div>
-  );
-}
